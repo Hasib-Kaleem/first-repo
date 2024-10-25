356926b307b69becb3e40b62d2bc00e187d8232d
Haseebullah Kaleem <hasibullah.kaleem@gmail.com>
2024-10-25 12:20:56 +0200

    25102024

---
+++ src/main/java/second_git_package/Practice01.java
@@ -0,0 +1,14 @@
+package second_git_package;
+
+public class Practice01 {
+    public static void main(String[] args) {
+
+        int a =12;
+        int b= 3;
+        if (a>b){
+            System.out.println("A is greater then b");
+        }else {
+            System.out.println("B is greater then A");
+        }
+    }
+}
