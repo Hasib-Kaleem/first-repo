7f0637f92002c91356ee45718eed4e8afe316e27
Haseebullah Kaleem <hasibullah.kaleem@gmail.com>
2024-10-25 12:45:22 +0200

    v2

--- src/main/java/second_git_package/Practice01.java
+++ src/main/java/second_git_package/Practice01.java
@@ -3,6 +3,9 @@ package second_git_package;
 public class Practice01 {
     public static void main(String[] args) {
 
+        System.out.println("Hello");
+        System.out.println("How are you");
+
 
     }
 }
